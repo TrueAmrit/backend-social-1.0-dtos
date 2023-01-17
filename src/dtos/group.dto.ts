55870b1a8b965a1e47a6cb017f58847924d87baf
Amrit Gupta <amritgupta1018@gmail.com>
2023-01-17 15:11:33 +0530

    updated

--- src/dtos/group.dto.ts
+++ src/dtos/group.dto.ts
@@ -1,11 +1,12 @@
 import { GroupType } from '../enums/groupType.enum';
 import { ContentDto } from './content.dto';
 import { DtoBase } from './dtoBase';
+import { UserDto } from './user.dto';
 
 export class GroupDto extends DtoBase {
   tittle?: string;
 
   type?: GroupType;
 
-  content?: ContentDto;
+  users?: UserDto[];
 }
